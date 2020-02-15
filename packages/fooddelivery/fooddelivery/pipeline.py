32d9f48a26fc15c5a9f4591f4a5ec7fc3db966ea
mohankumar1905 <mohankumar1905@gmail.com>
2020-02-15 15:15:38 +0530

    only 10 estimators

--- packages/fooddelivery/fooddelivery/pipeline.py
+++ packages/fooddelivery/fooddelivery/pipeline.py
@@ -2,8 +2,6 @@ from imblearn.pipeline import Pipeline
 import pandas as pd
 from category_encoders import OrdinalEncoder
 from sklearn.preprocessing import LabelEncoder
-#from imblearn.over_sampling import RandomOverSampler
-#from lightgbm import LGBMClassifier
 from fooddelivery.datamanagement import preprocessing as pp
 from fooddelivery.datamanagement import feature_extraction as fx
 from fooddelivery.config import config as cg
@@ -22,7 +20,6 @@ food_delivery_pipe = Pipeline(
 	('inherit_old_restaurant_info', fx.FillNewRestaurantDetails(variables=cg.INHERITING_INFO_COLUMNS)),
 	('extract_cusine_varities_info', fx.CusineVarities(variable=cg.CUSINE_VARIABLE)),
 	('encoding', fx.EncodeCategoricalVariables(variables=cg.ORDINAL_ENCODING_COLUMNS)),
-	#('random_over_sampling', RandomOverSampler(random_state=1994)),
-	('lightgbm', fx.Balanced_Lightgbm_Model(n_estimators = 3000, random_state = 1994, learning_rate = 0.1, objective = 'multiclass',
+	('lightgbm', fx.Balanced_Lightgbm_Model(n_estimators = 10, random_state = 1994, learning_rate = 0.1, objective = 'multiclass',
 	 eval_metric='multi_logloss', early_stopping_rounds = 200, verbose = 1))
 	])
