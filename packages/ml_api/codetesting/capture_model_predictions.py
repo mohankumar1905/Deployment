ef89957a316a2360f60abf3fea65580892d8f464
mohankumar1905 <mohankumar1905@gmail.com>
2020-02-16 13:16:27 +0530

    circleci github testing

--- packages/ml_api/codetesting/capture_model_predictions.py
+++ packages/ml_api/codetesting/capture_model_predictions.py
@@ -12,10 +12,12 @@ from fooddelivery.datamanagement.dataloadersaver import load_dataset
 from api import config
 
 
-def capture_predictions():
+def capture_predictions(
+        *,
+        save_file: str = 'test_data_predictions.xlsx'):
     """Save the test data predictions to a Excel."""
 
-    test_data = load_dataset(filename=config.TESTING_DATA_FILE)
+    test_data = load_dataset(filename='Data_Test.xlsx')
 
     # we take a slice with no input validation issues
     multiple_test_json = test_data[99:600].to_json(orient='records')
@@ -29,8 +31,8 @@ def capture_predictions():
     # hack here to save the file to the fooddelivery model
     # package of the repo, not the installed package
     predictions_df.to_excel(
-        f'{config.PACKAGE_ROOT.parent}/fooddelivery/fooddelivery/datasets/{config.PREVIOUS_TEST_FILE}')
-
+        f'{config.PACKAGE_ROOT.parent}/fooddelivery/fooddelivery/datasets/{save_file}')
+    print
 
 if __name__ == '__main__':
     capture_predictions()
--- packages/ml_api/codetesting/differential_tests/test_differential.py
+++ packages/ml_api/codetesting/differential_tests/test_differential.py
@@ -5,20 +5,22 @@ import pytest
 from fooddelivery.config import config
 from fooddelivery.predict import make_prediction
 from fooddelivery.datamanagement.dataloadersaver import load_dataset
+from api import config as api_config
 
 @pytest.mark.differential
-def test_model_prediction_differential():
+def test_model_prediction_differential(
+        *,
+        save_file='test_data_predictions.xlsx'):
     """
     This test compares the prediction result similarity of
     the current model with the previous model's results.
     """
-    
+    # Given
+    previous_model_df = pd.read_excel(f'{api_config.PACKAGE_ROOT.parent}/fooddelivery/fooddelivery/datasets/{save_file}')
+    previous_model_predictions = previous_model_df.predictions.values
+
     test_data = load_dataset(filename=config.TESTING_DATA_FILE)
     multiple_test_json = test_data[99:600].to_json(orient='records')
-
-    # Given
-    previous_model_df = load_dataset(filename=config.PREVIOUS_TEST_FILE)
-    previous_model_predictions = previous_model_df.predictions.values
 
     # When
     response = make_prediction(input_data=json.loads(multiple_test_json))
