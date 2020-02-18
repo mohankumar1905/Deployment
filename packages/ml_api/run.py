74bc410b2ff19c04899334a9800ca76fe68b4c4f
mohankumar1905 <mohankumar1905@gmail.com>
2020-02-18 09:45:50 +0530

    proc file added

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,7 +1,7 @@
 from api.app import create_app
-from api.config import DevelopmentConfig
+from api.config import DevelopmentConfig, ProductionConfig
 
-application = create_app(config_object=DevelopmentConfig)
+application = create_app(config_object=ProductionConfig)
 
 if __name__ == '__main__':
     """ __name__ will __main__ for the python scripts we execute directly.
